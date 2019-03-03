c25879af8f5338829e092c6a94c08643e9af894a
u-blavins <usama.blavins1@gmail.com>
2019-03-03 21:37:18 +0000

    Update kms.py and test_kms.py with methods for listing keys + aliases

--- infrastructure_testing/kms.py
+++ infrastructure_testing/kms.py
@@ -1,10 +1,46 @@
-import json
+"""Utility class for KMS"""
 
 
 class KMS:
-    """ Class for testing KMS"""
+    """Utility class for KMS"""
 
-    def __init__(self, key, policy):
-        """ Initialise Object """
-        self.key = key
-        self.policy = policy
+    def __init__(self, client):
+        """Initialise KMS Class"""
+        self.client = client
+
+    def get_keys(self):
+        """
+        Make an AWS call to list keys within the account
+
+        Returns:
+            keys(list): list of kms keys
+        """
+        keys = []
+        try:
+            paginator = self.client.get_paginator('list_keys')
+            response_iter = paginator.paginate()
+
+            for page in response_iter:
+                for key in page['Keys']:
+                    keys.append(key)
+        except Exception as error:
+            raise error
+        return keys
+
+    def get_key_aliases(self, key_id=''):
+        """
+        Make an AWS call to list key aliases' within the
+        account
+
+        Args:
+            key_id(str): kms key id
+        Returns:
+            aliases(dict): JSON response containg key alias
+        """
+        aliases = []
+        if key_id != '':
+            if isinstance(key_id, list):
+                print('Do Something')
+            else:
+                raise TypeError('param (key_id) type must be a list')
+        return aliases
--- tests/infrastructure_testing/test_kms.py
+++ tests/infrastructure_testing/test_kms.py
@@ -1,12 +1,51 @@
 import pytest
-from mock import MagicMock
-from infrastructure_testing.main import *
+from mock import MagicMock, patch
+
+from infrastructure_testing.kms import KMS
 
 
 class TestMain:
-    """ Test Suite for main.py """
+    """ Test Suite for KMS Utility class """
 
     def setup_method(self):
-        self.policy = {
-            'Test': 'test'
-        }
+        self.mock_client = MagicMock()
+        self.mock_kms = KMS(self.mock_client)
+
+    def test_get_keys_returns_keys(self):
+        """ Test Success: Keys returned """
+        fake_key_details = {'KeyId': 'test-key',
+                            'KeyArn': 'test-key-arn'}
+
+        fake_key = {'Keys': [fake_key_details]}
+        fake_response = [fake_key]
+        self.mock_client.get_paginator.return_value.paginate.return_value = \
+            fake_response
+
+        sut = self.mock_kms.get_keys()
+
+        assert len(sut) == 1
+        assert sut == [fake_key_details]
+
+    def test_get_keys_raises_excpetion(self):
+        """ Test Failure: Exception is raised if pagination fails """
+        fake_response = '{"HttpResponseCode": 400}'
+        self.mock_client.get_paginator.return_value.paginate.return_value = \
+            fake_response
+        with pytest.raises(TypeError) as exc_info:
+            self.mock_kms.get_keys()
+
+        assert 'TypeError' in str(exc_info)
+
+    def test_get_key_aliases_raises_correct_exception(self):
+        """ Test Failure: TypeError raised when param is not a list """
+        fake_key_id = 'test-key'
+        err_message = 'param (key_id) type must be a list'
+        with pytest.raises(TypeError) as exc_info:
+            self.mock_kms.get_key_aliases(fake_key_id)
+
+        assert type(exc_info.value) != type(err_message)
+        assert str(exc_info.value) == err_message
+
+
+if __name__ == '__main__':
+    pytest.main()
